a76d50c9f7a760706c187ddde8641653f7566296
John Salaveria <jj.salaveria@gmail.com>
2016-09-30 14:07:13 -0400

    created profile view, made components folder

--- index.ios.js
+++ index.ios.js
@@ -12,42 +12,25 @@ import {
   View
 } from 'react-native';
 
+import ProfileView from './components/ProfileView';
+
+var items = [
+  {
+    title: 'Beasts & Mans',
+    author: 'Mike Bernstein'
+  },
+  {
+    title: '1984',
+    author: 'George Orwell'
+  }
+];
+
 class BookshelfMobile extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <ProfileView items={items}/>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('BookshelfMobile', () => BookshelfMobile);
