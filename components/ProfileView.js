1193b512501271731dadd0ab8c4cd5def6303222
John Salaveria <jj.salaveria@gmail.com>
2016-10-01 15:43:16 -0400

    updated profile badge

--- components/ProfileView.js
+++ components/ProfileView.js
@@ -1,11 +1,11 @@
-'use strict';
-
 import React, { Component } from 'react';
+import Hr from 'react-native-hr';
 
 import {
   StyleSheet,
   Text,
-  View
+  View,
+  TouchableHighlight
 } from 'react-native';
 
 export default class ProfileView extends Component {
@@ -16,12 +16,48 @@ export default class ProfileView extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text> Hi!</Text>
-        {
-          this.props.items.map( (item, i) => (
-            <BookItem title={item.title}/>
-          ))
-        }
+        <ProfileBadge/>
+        <Hr style={styles.hr} lineColor='#dddddd'/ >
+        <BorrowedBooks borrowed='5'/>
+      </View>
+    );
+  }
+}
+
+class BorrowedBooks extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={{marginTop: 20, alignSelf: 'stretch'}}>
+        <Text style={{fontSize: 20, marginBottom: 5}}>Currently Borrowed</Text>
+        <Text style={styles.h4}> {this.props.borrowed} in total</Text>
+      </View>
+    );
+  }
+}
+
+class ProfileBadge extends Component {
+  constructor (props) {
+    super(props);
+  }
+
+  _onPressButton() {
+    console.log('pressed!');
+  }
+
+  render() {
+    return (
+      <View style={styles.profilebadge}>
+        <TouchableHighlight onPress={this._onPressButton}>
+          <View style={{width: 60, height: 60, backgroundColor: 'red', borderRadius: 60}} />
+        </TouchableHighlight>
+        <View style={styles.texts}>
+          <Text style= {{fontSize: 26, marginBottom: 5}}>John Salaveria</Text>
+          <Text style= {styles.h4} >@johnsington</Text>
+        </View>
       </View>
     );
   }
@@ -46,18 +82,27 @@ class BookItem extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'column',
+    padding: 10,
+    alignItems: 'flex-start'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  texts: {
+    paddingLeft: 10,
+    justifyContent: 'center'
+  },
+  profilebadge: {
+    flexDirection: 'row',
+    height: 60,
+    alignSelf: 'stretch',
+    marginBottom: 20
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
+  h4: {
+    fontSize: 16,
+    color: '#828282'
+  }
 });
